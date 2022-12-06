56e1c4b913e371186908bf4a77e99a2584f3a628
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-12-06 10:05:16 +0100

    day 6

--- 2022/06/day06.test.ts
+++ 2022/06/day06.test.ts
@@ -1,17 +1,52 @@
 import { assert, describe, it } from "vitest";
-import { solve06a, solve06b } from "./day06";
+import { fileToCharArray, solve06a, solve06b } from "./day06";
 
 describe("day06", () => {
-  it("input", () => {
-    const sampleData = [0];
-    assert.deepEqual(sampleData, [0]);
+  it("fileToCharArray", () => {
+    assert.deepEqual(fileToCharArray("06/sample.txt"), fileToCharArrayFixture);
   });
   it("solve06a", () => {
-    const sampleData = [0];
-    assert.equal(solve06a(sampleData), 0);
+    const sampleData = fileToCharArray("06/sample.txt");
+    assert.equal(solve06a(sampleData), 10);
   });
   it("solve06b", () => {
-    const sampleData = [0];
-    assert.equal(solve06b(sampleData), 0);
+    const sampleData = fileToCharArray("06/sample.txt");
+    assert.equal(solve06b(sampleData), 29);
   });
 });
+
+const fileToCharArrayFixture = [
+  "n",
+  "z",
+  "n",
+  "r",
+  "n",
+  "f",
+  "r",
+  "f",
+  "n",
+  "t",
+  "j",
+  "f",
+  "m",
+  "v",
+  "f",
+  "w",
+  "m",
+  "z",
+  "d",
+  "f",
+  "j",
+  "l",
+  "v",
+  "t",
+  "q",
+  "n",
+  "b",
+  "h",
+  "c",
+  "p",
+  "r",
+  "s",
+  "g",
+];
--- 2022/06/day06.ts
+++ 2022/06/day06.ts
@@ -1,15 +1,28 @@
 import fs from "fs";
 
-const inputA = [0];
-const inputB = [0];
-
-export function solve06a(input: number[]) {
-  return 0;
+export function fileToCharArray(file: string) {
+  return fs.readFileSync(file, "utf8").split("");
 }
 
-export function solve06b(input: number[]) {
-  return 0;
+const inputAB = fileToCharArray("06/input.txt");
+
+function firstSetOfXUniqueCharacters(input: string[], uniqueCharCount: number) {
+  for (let i = uniqueCharCount; i <= input.length; i++) {
+    const lastN = new Set(input.slice(i - uniqueCharCount, i));
+    if (lastN.size === uniqueCharCount) {
+      return i;
+    }
+  }
+  return -1;
 }
 
-console.log(solve06a(inputA));
-console.log(solve06b(inputB));
+export function solve06a(input: string[]) {
+  return firstSetOfXUniqueCharacters(input, 4);
+}
+
+export function solve06b(input: string[]) {
+  return firstSetOfXUniqueCharacters(input, 14);
+}
+
+console.log(solve06a(inputAB));
+console.log(solve06b(inputAB));
