0dc4e9bd32475e6ac86cff76f270a573b831f592
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-12-09 13:03:32 +0100

    clean day 7 a bit

--- 2022/07/day07.test.ts
+++ 2022/07/day07.test.ts
@@ -1,10 +1,25 @@
 import { assert, describe, it } from "vitest";
-import { solve07a, solve07b } from "./day07";
+import { FileSystem, solve07a, solve07b } from "./day07";
 
 describe("day07", () => {
   it("input", () => {
-    const sampleData = [0];
-    assert.deepEqual(sampleData, [0]);
+    const expectedOutput = `- / (dir)
+  - b.txt (file, size=14848514)
+  - c.dat (file, size=8504156)
+  - a (dir)
+    - f (file, size=29116)
+    - g (file, size=2557)
+    - h.lst (file, size=62596)
+    - e (dir)
+      - i (file, size=584)
+  - d (dir)
+    - d.ext (file, size=5626152)
+    - d.log (file, size=8033020)
+    - j (file, size=4060174)
+    - k (file, size=7214296)
+`;
+    const sampleFileSystem = new FileSystem("07/sample.txt");
+    assert.equal(sampleFileSystem.printDir(), expectedOutput);
   });
   it("solve07a", () => {
     const sampleData = [0];
--- 2022/07/day07.ts
+++ 2022/07/day07.ts
@@ -13,9 +13,9 @@ type Directory = {
   subdirs: Directory[];
 };
 
-class FileSystem {
-  root: Directory;
-  pwd: Directory;
+export class FileSystem {
+  private root: Directory;
+  private pwd: Directory;
   FILESYSTEM_SIZE = 70_000_000;
 
   constructor(file: string) {
@@ -23,23 +23,19 @@ class FileSystem {
     this.root = { name: "/", files: [], parent: null, subdirs: [] };
     this.pwd = this.root;
     this.processCommands(commands);
-    this.printDir(this.root);
   }
 
   private processCommands(commands: string[]) {
     commands.forEach((command) => {
       this.processCommand(command, this.pwd, this.root);
-      console.log("command: " + command);
     });
     this.pwd = this.root;
   }
 
   private processCommand(command: string, pwd: Directory, root: Directory) {
-    console.log("processing", command, pwd.name, root.name);
     const cmd = command.split(" ");
     switch (cmd[0]) {
       case "dir":
-        // create a new subfolder
         const newFolder = {
           name: cmd[1],
           files: [],
@@ -49,22 +45,20 @@ class FileSystem {
         this.pwd.subdirs.push(newFolder);
         break;
       case cmd[0].match(/\d+/)?.input:
-        // create a new file
         const newFile = { dir: pwd, name: cmd[1], size: parseInt(cmd[0]) };
         this.pwd.files.push(newFile);
         break;
       case "$":
-        if (cmd[1] === "ls") {
-          break;
-        }
         if (cmd[1] === "cd") {
-          if (cmd[2] === "..") {
-            this.pwd = pwd.parent ?? pwd;
-          } else if (cmd[2] === "/") {
-            this.pwd = root;
-          } else {
-            this.pwd = pwd.subdirs.find((dir) => dir.name === cmd[2]) ?? pwd;
-            console.log("new pwd", pwd.name);
+          switch (cmd[2]) {
+            case "..":
+              this.pwd = pwd.parent ?? pwd;
+              break;
+            case "/":
+              this.pwd = root;
+              break;
+            default:
+              this.pwd = pwd.subdirs.find((dir) => dir.name === cmd[2]) ?? pwd;
           }
         }
         break;
@@ -73,7 +67,7 @@ class FileSystem {
     }
   }
 
-  public dirSize(dir: Directory) {
+  public dirSize(dir: Directory): number {
     const files = this.getFilesRecursiveFlat(dir);
     const size = files.reduce((acc, cur) => acc + cur.size, 0);
     return size;
@@ -87,13 +81,17 @@ class FileSystem {
     return [currentDir, ...childDirs];
   }
 
-  private getFilesRecursiveFlat(dir: Directory) {
-    // get files
+  private getFilesRecursiveFlat(dir: Directory): File[] {
     const currentDirFiles = dir.files;
     const childFiles = dir.subdirs
       .map((dir) => this.getFilesRecursiveFlat(dir))
-      .flat() as File[];
+      .flat();
     return [...currentDirFiles, ...childFiles];
+  }
+
+  public getSpaceUsed(dir: Directory = this.root) {
+    const files = this.getFilesRecursiveFlat(dir);
+    return files.reduce((acc, cur) => acc + cur.size, 0);
   }
 
   public getSmallestDirToDelete(requiredSpace: number): Directory | undefined {
@@ -107,68 +105,44 @@ class FileSystem {
     return dirToDelete;
   }
 
-  public getSpaceUsed(dir: Directory = this.root) {
-    const files = this.getFilesRecursiveFlat(dir);
-    const usedSpace = files.reduce((acc, cur) => acc + cur.size, 0);
-    return usedSpace;
-  }
-
-  // TODO use orderDirsBySize for this & below
-  public findSmallDirs() {
-    const smallDirs = [] as { dir: Directory; size: number }[];
-    this.findSmallDirsRecursive(this.root, smallDirs);
-    console.log(
-      `--fsd done, dirs: ${smallDirs.map((dir) => dir.size).join(",")}`
+  public findSmallDirs(maxSize: number = 100_000) {
+    const smallDirs = this.getDirsRecursiveFlat().filter(
+      (dir) => this.dirSize(dir) < maxSize
     );
-    return smallDirs.reduce((acc, cur) => acc + cur.size, 0);
+    return smallDirs.reduce((acc, cur) => acc + this.dirSize(cur), 0);
   }
 
-  // TODO use orderDirsBySize for this
-  private findSmallDirsRecursive(
-    dir: Directory,
-    smallDirs: { dir: Directory; size: number }[]
-  ) {
-    // TODO: count dir size recursively instead!
-    console.log(`--fsdr - dir: ${dir.name}, smallDirs: ${smallDirs}`);
-    const size = this.dirSize(dir);
-    if (size <= 100000) {
-      smallDirs.push({ dir, size });
-    }
-    dir.subdirs.forEach((dir) => this.findSmallDirsRecursive(dir, smallDirs));
-  }
-
-  // TODO: port generation to the class
-
-  public printDir(dir: Directory = this.root, depth = 0) {
+  public printDir(dir: Directory = this.root, depth = 0, output = ""): string {
     const sortedSubfolders = dir.subdirs.sort((a, b) =>
       a.name < b.name ? -1 : 1
     );
     const sortedSubfiles = dir.files.sort((a, b) => (a.name < b.name ? -1 : 1));
-    console.log(`${"  ".repeat(depth)}- ${dir.name} (dir)`);
-    sortedSubfiles.forEach((file) =>
-      console.log(
-        `${"  ".repeat(depth + 1)}- ${file.name} (file, size=${file.size})`
-      )
+    output += `${"  ".repeat(depth)}- ${dir.name} (dir)\n`;
+    sortedSubfiles.forEach(
+      (file) =>
+        (output += `${"  ".repeat(depth + 1)}- ${file.name} (file, size=${
+          file.size
+        })\n`)
     );
-    sortedSubfolders.forEach((folder) => this.printDir(folder, depth + 1));
+    sortedSubfolders.forEach(
+      (folder) => (output += this.printDir(folder, depth + 1))
+    );
+    return output;
   }
 }
 
-export function solve07a(file: string) {
-  const inputA = new FileSystem(file);
-  const smallFileSum = inputA.findSmallDirs();
+export function solve07a(fileSystem: FileSystem) {
+  const smallFileSum = fileSystem.findSmallDirs();
   return smallFileSum;
 }
 
-export function solve07b(file: string) {
-  const inputB = new FileSystem(file);
-  const dirToDelete = inputB.getSmallestDirToDelete(30_000_000);
-  console.log(inputB.dirSize(dirToDelete));
+export function solve07b(fileSystem: FileSystem) {
+  const dirToDelete = fileSystem.getSmallestDirToDelete(
+    30_000_000
+  ) as Directory;
+  return fileSystem.dirSize(dirToDelete);
 }
 
-console.log(solve07a("07/input.txt"));
-solve07b("07/input.txt");
-
-/**
- * TODO: clean this up & write tests
- */
+const fileSystem = new FileSystem("07/input.txt");
+console.log(solve07a(fileSystem));
+console.log(solve07b(fileSystem));
