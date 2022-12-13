0b0309adf7e042dff877eaa345ef926b1c83dbdd
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-12-13 18:22:08 +0100

    day 13 v1

--- 2022/13/day13.test.ts
+++ 2022/13/day13.test.ts
@@ -1,17 +1,29 @@
 import { assert, describe, it } from "vitest";
-import { solve13a, solve13b } from "./day13";
+import { parseDistressSignal, solve13a, solve13b } from "./day13";
+
+const packetFixture = [
+  { left: [1, 1, 3, 1, 1], right: [1, 1, 5, 1, 1] },
+  { left: [[1], [2, 3, 4]], right: [[1], 4] },
+  { left: [9], right: [[8, 7, 6]] },
+  { left: [[4, 4], 4, 4], right: [[4, 4], 4, 4, 4] },
+  { left: [7, 7, 7, 7], right: [7, 7, 7] },
+  { left: [], right: [3] },
+  { left: [[[]]], right: [[]] },
+  {
+    left: [1, [2, [3, [4, [5, 6, 7]]]], 8, 9],
+    right: [1, [2, [3, [4, [5, 6, 0]]]], 8, 9],
+  },
+];
 
 describe("day13", () => {
   it("input", () => {
-    const sampleData = [0];
-    assert.deepEqual(sampleData, [0]);
+    const sampleData = parseDistressSignal("13/sample.txt");
+    assert.deepEqual(sampleData, packetFixture);
   });
   it("solve13a", () => {
-    const sampleData = [0];
-    assert.equal(solve13a(sampleData), 0);
+    assert.equal(solve13a("13/sample.txt"), 13);
   });
   it("solve13b", () => {
-    const sampleData = [0];
-    assert.equal(solve13b(sampleData), 0);
+    assert.equal(solve13b("13/sample.txt"), -99);
   });
 });
--- 2022/13/day13.ts
+++ 2022/13/day13.ts
@@ -3,13 +3,142 @@ import fs from "fs";
 const inputA = [0];
 const inputB = [0];
 
-export function solve13a(input: number[]) {
-  return 0;
+type Packet = Array<PacketUnit>;
+type PacketUnit = Packet | number;
+
+export function parseDistressSignal(file: string) {
+  const data = fs
+    .readFileSync(file, "utf-8")
+    .split("\n\n")
+    .map((packetPair) => {
+      const [left, right] = packetPair
+        .split("\n")
+        .map((packet) => JSON.parse(packet) as Packet);
+      return { left, right };
+    });
+  return data;
 }
 
-export function solve13b(input: number[]) {
-  return 0;
+function enterCompareInput({
+  left,
+  right,
+}: {
+  left: PacketUnit;
+  right: PacketUnit;
+}) {
+  const result = compareInputs({ left, right });
+  return result >= 0;
 }
 
-console.log(solve13a(inputA));
-console.log(solve13b(inputB));
+function compareInputs({
+  left,
+  right,
+  depth = 0,
+}: {
+  left: PacketUnit;
+  right: PacketUnit;
+  depth?: number;
+}): number {
+  console.log(
+    `${"  ".repeat(depth)}- Compare ${JSON.stringify(left)} vs ${JSON.stringify(
+      right
+    )}`
+  );
+  // if both are ints, left should be lower than right
+  // if they're equal, continue
+  if (typeof left === "number" && typeof right === "number") {
+    const res = right - left;
+    if (res < 0) {
+      console.log(
+        `${"  ".repeat(
+          depth
+        )}- Right side is smaller, so inputs _are not_ in the right order`
+      );
+    }
+    if (res > 0) {
+      console.log(
+        `${"  ".repeat(
+          depth
+        )}- Left side is smaller, so inputs _are_ in the right order`
+      );
+    }
+    return right - left;
+  }
+
+  // if both are arrays, compare them recursively
+  if (Array.isArray(left) && Array.isArray(right)) {
+    let result = 0;
+    while (left.length || right.length) {
+      const leftItem = left.shift();
+      const rightItem = right.shift();
+      if ((leftItem || leftItem === 0) && (rightItem || rightItem === 0)) {
+        result = compareInputs({
+          left: leftItem,
+          right: rightItem,
+          depth: depth + 1,
+        });
+      } else if (leftItem && !rightItem) {
+        console.log(
+          `${"  ".repeat(
+            depth + 1
+          )}- Right side ran out of items, so inputs _are not_ in the right order`
+        );
+        result = -1;
+      } else if (!leftItem && rightItem) {
+        console.log(
+          `${"  ".repeat(
+            depth + 1
+          )}- Left side ran out of items, so inputs _are_ in the right order`
+        );
+        result = 1;
+      }
+      if (result !== 0) {
+        return result;
+      }
+    }
+    return result;
+  }
+
+  // if one is an array and the other is an int, compare them as arrays
+  if (Array.isArray(left) && typeof right === "number") {
+    console.log(
+      `${"  ".repeat(
+        depth
+      )}- Mixed types; convert right to [${right}] and retry comparison`
+    );
+    return compareInputs({ left, right: [right], depth: depth + 1 });
+  }
+  if (typeof left === "number" && Array.isArray(right)) {
+    console.log(
+      `${"  ".repeat(
+        depth
+      )}- Mixed types; comvert left to [${left}] and retry comparison`
+    );
+    return compareInputs({ left: [left], right, depth: depth + 1 });
+  }
+
+  throw new Error("Invalid input");
+}
+
+export function solve13a(file: string) {
+  const signal = parseDistressSignal(file);
+  const results = [] as number[];
+  signal.forEach((item, idx) => {
+    if (idx < 30) {
+      console.log(`\n== Pair ${idx + 1} ==`);
+      if (enterCompareInput({ left: item.left, right: item.right })) {
+        results.push(idx + 1);
+      }
+    }
+  });
+  console.log("result", results);
+  const result = results.reduce((acc, item) => acc + item, 0);
+  return result;
+}
+
+export function solve13b(file: string) {
+  const signal = parseDistressSignal(file);
+  return -1;
+}
+
+console.log(solve13a("13/input2.txt"));
