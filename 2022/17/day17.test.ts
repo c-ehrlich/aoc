72ec17bd717ae0087c275429fc98cd33e058e70d
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-12-17 14:07:02 +0100

    day 17 part 1

--- 2022/17/day17.test.ts
+++ 2022/17/day17.test.ts
@@ -1,17 +1,75 @@
 import { assert, describe, it } from "vitest";
-import { solve17a, solve17b } from "./day17";
+import { Chamber, Piece, RocksFalling, solve17a, solve17b } from "./day17";
 
 describe("day17", () => {
-  it("input", () => {
-    const sampleData = [0];
-    assert.deepEqual(sampleData, [0]);
+  it("canGoThere", () => {
+    const chamber = new RocksFalling("17/sample.txt");
+    // start
+    assert.equal(
+      chamber.canGoThere({
+        chamber: canGoThereChamberFixture,
+        piece: canGoTherePieceFixture,
+        offset: [2, 4],
+      }),
+      true
+    );
+    // up
+    assert.equal(
+      chamber.canGoThere({
+        chamber: canGoThereChamberFixture,
+        piece: canGoTherePieceFixture,
+        offset: [3, 4],
+      }),
+      true
+    );
+    // right
+    assert.equal(
+      chamber.canGoThere({
+        chamber: canGoThereChamberFixture,
+        piece: canGoTherePieceFixture,
+        offset: [2, 5],
+      }),
+      false
+    );
+    // down
+    assert.equal(
+      chamber.canGoThere({
+        chamber: canGoThereChamberFixture,
+        piece: canGoTherePieceFixture,
+        offset: [1, 4],
+      }),
+      false
+    );
+    // left
+    assert.equal(
+      chamber.canGoThere({
+        chamber: canGoThereChamberFixture,
+        piece: canGoTherePieceFixture,
+        offset: [2, 4],
+      }),
+      true
+    );
   });
   it("solve17a", () => {
-    const sampleData = [0];
-    assert.equal(solve17a(sampleData), 0);
+    assert.equal(solve17a("17/sample.txt"), 3068);
   });
-  it("solve17b", () => {
-    const sampleData = [0];
-    assert.equal(solve17b(sampleData), 0);
-  });
+  // it("solve17b", () => {
+  //   const sampleData = [0];
+  //   assert.equal(solve17b(sampleData), 0);
+  // });
 });
+
+const canGoThereChamberFixture: Chamber = [
+  ["#", "#", "#", "#", "#", "#", "#"],
+  ["#", "#", "#", "#", "#", ".", "#"],
+  ["#", "#", "#", "#", "#", ".", "."],
+  ["#", "#", "#", ".", ".", ".", "."],
+  ["#", "#", "#", "#", ".", ".", "."],
+  ["#", "#", "#", "#", "#", ".", "#"],
+  ["#", "#", "#", "#", "#", "#", "#"],
+];
+const canGoTherePieceFixture: Piece = [
+  [".", "#", "."],
+  ["#", "#", "#"],
+  [".", "#", "."],
+];
--- 2022/17/day17.ts
+++ 2022/17/day17.ts
@@ -1,15 +1,165 @@
 import fs from "fs";
 
-const inputA = [0];
-const inputB = [0];
+type Wind = "<" | ">";
+type ChamberRow = ("." | "#")[];
+export type Chamber = ChamberRow[];
+const pieceWide = [["#", "#", "#", "#"]] as const;
+type PieceWide = typeof pieceWide;
+const pieceTall = [["#"], ["#"], ["#"], ["#"]] as const;
+type PieceTall = typeof pieceTall;
+const pieceCross = [
+  [".", "#", "."],
+  ["#", "#", "#"],
+  [".", "#", "."],
+] as const;
+type PieceCross = typeof pieceCross;
+const pieceSquare = [
+  ["#", "#"],
+  ["#", "#"],
+] as const;
+type PieceSquare = typeof pieceSquare;
+const pieceL = [
+  ["#", "#", "#"],
+  [".", ".", "#"],
+  [".", ".", "#"],
+] as const; // bottom is index 0
+type PieceL = typeof pieceL;
+const pieceRotation = [
+  pieceWide,
+  pieceCross,
+  pieceL,
+  pieceTall,
+  pieceSquare,
+] as const;
+export type Piece = typeof pieceRotation[number];
+export type Offset = [number, number];
 
-export function solve17a(input: number[]) {
+export class RocksFalling {
+  PIECES_TO_DROP = 2022;
+  wind: Wind[];
+  mod: number;
+  chamber: Chamber;
+
+  constructor(file: string) {
+    this.wind = fs.readFileSync(file, "utf8").split("") as Wind[];
+    this.mod = this.wind.length;
+    this.chamber = [
+      ["#", "#", "#", "#", "#", "#", "#"],
+      ...new Array(4 * this.PIECES_TO_DROP)
+        .fill(0)
+        .map(() => [".", ".", ".", ".", ".", ".", "."] as ChamberRow),
+    ];
+  }
+
+  private findHighestRowWithPiece(): number {
+    for (let i = this.chamber.length - 1; i >= 0; i--) {
+      if (this.chamber[i].includes("#")) {
+        return i;
+      }
+    }
+    return -1; // will never reach because the bottom is
+  }
+
+  public dropPieces() {
+    let move = 0;
+    for (let i = 0; i < this.PIECES_TO_DROP; i++) {
+      if (i < 10) {
+        console.log(
+          this.chamber
+            .slice(0, 20)
+            .reverse()
+            .map((row) => row.join(""))
+        );
+      }
+      const piece = pieceRotation[i % 5];
+      const bottomLine = this.findHighestRowWithPiece();
+      const offset: Offset = [bottomLine + 4, 2];
+      let pieceIsLocked = false;
+
+      while (!pieceIsLocked) {
+        const windDir = this.wind[move % this.mod];
+        move++;
+
+        // try to move the piece sideways
+        if (
+          windDir === "<" &&
+          this.canGoThere({
+            chamber: this.chamber,
+            piece,
+            offset: [offset[0], offset[1] - 1],
+          })
+        ) {
+          offset[1]--;
+        } else if (
+          windDir === ">" &&
+          this.canGoThere({
+            chamber: this.chamber,
+            piece,
+            offset: [offset[0], offset[1] + 1],
+          })
+        ) {
+          offset[1]++;
+        }
+
+        // try to move the piece down
+        if (
+          this.canGoThere({
+            chamber: this.chamber,
+            piece,
+            offset: [offset[0] - 1, offset[1]],
+          })
+        ) {
+          offset[0]--;
+        } else {
+          pieceIsLocked = true;
+          // write the piece to the chamber
+          for (let y = 0; y < piece.length; y++) {
+            for (let x = 0; x < piece[y].length; x++) {
+              if (piece[y][x] === "#") {
+                this.chamber[offset[0] + y][offset[1] + x] = "#";
+              }
+            }
+          }
+        }
+      }
+    }
+    return this.findHighestRowWithPiece();
+  }
+
+  public canGoThere({
+    chamber,
+    piece,
+    offset,
+  }: {
+    chamber: Chamber;
+    piece: Piece;
+    offset: Offset;
+  }) {
+    for (let y = 0; y < piece.length; y++) {
+      for (let x = 0; x < piece[y].length; x++) {
+        if (
+          (piece[y][x] === "#" &&
+            chamber[offset[0] + y][offset[1] + x] === "#") ||
+          piece[0].length + offset[1] > chamber[0].length ||
+          offset[1] < 0 ||
+          offset[0] < 0
+        ) {
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+}
+
+export function solve17a(file: string) {
+  const rf = new RocksFalling(file);
+  return rf.dropPieces();
+}
+
+export function solve17b(file: string) {
   return 0;
 }
 
-export function solve17b(input: number[]) {
-  return 0;
-}
-
-console.log(solve17a(inputA));
-console.log(solve17b(inputB));
+console.log(solve17a("17/input.txt"));
+// console.log(solve17b(inputB));
