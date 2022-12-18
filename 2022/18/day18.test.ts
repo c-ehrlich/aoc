f61ea158eb42f1df2bf7a12968c686f029051dbf
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-12-18 11:16:42 +0100

    day 18 part 2

--- 2022/18/day18.test.ts
+++ 2022/18/day18.test.ts
@@ -1,15 +1,14 @@
 import { assert, describe, it } from "vitest";
-import { solve18a, solve18b } from "./day18";
+import { solve18a, solve18b, testCubeArea } from "./day18";
 
 describe("day18", () => {
-  // it("input", () => {
-  //   const sampleData = [0];
-  //   assert.deepEqual(sampleData, [0]);
-  // });
   it("solve18a", () => {
     assert.equal(solve18a("18/sample.txt"), 64);
   });
   it("solve18b", () => {
     assert.equal(solve18b("18/sample.txt"), 58);
+  });
+  it("cube area", () => {
+    assert.equal(testCubeArea("18/sample.txt"), 64);
   });
 });
--- 2022/18/day18.ts
+++ 2022/18/day18.ts
@@ -26,72 +26,176 @@ function findSurfaceArea(cubes: number[]) {
   return area;
 }
 
-function findSurfaceAreaWithoutPockets(rock: number[]) {
-  const MAX_X =
-    2 + rock.reduce((acc, cur) => Math.max(acc, Math.floor(cur / 10000)), 0);
-  const MAX_Y =
-    2 +
-    rock.reduce(
-      (acc, cur) => Math.max(acc, Math.floor((cur % 10000) / 100)),
-      0
-    );
-  const MAX_Z = 2 + rock.reduce((acc, cur) => Math.max(acc, cur % 100), 0);
+/**
+ * Our hack of turning each cube into a single number doesn't work for part 2
+ * Still leaving the part 1 answer above because I think it's neat
+ */
 
-  const steam = [] as number[];
+type CubeList = [number, number, number][];
+
+function parseCubesArray(file: string) {
+  return fs
+    .readFileSync(file, "utf8")
+    .split("\n")
+    .map((line) => line.split(",").map((n) => parseInt(n))) as CubeList;
+}
+
+function findSurfaceAreaWithoutPockets(droplet: CubeList) {
+  const MAX_X = Math.max(...droplet.map((cube) => cube[0])) + 1;
+  const MAX_Y = Math.max(...droplet.map((cube) => cube[1])) + 1;
+  const MAX_Z = Math.max(...droplet.map((cube) => cube[2])) + 1;
+  const MIN_X = Math.min(...droplet.map((cube) => cube[0])) - 1;
+  const MIN_Y = Math.min(...droplet.map((cube) => cube[1])) - 1;
+  const MIN_Z = Math.min(...droplet.map((cube) => cube[2])) - 1;
+  const limits = { MAX_X, MAX_Y, MAX_Z, MIN_X, MIN_Y, MIN_Z };
+
+  // create steam and measure its total surface area
+  const steam = [] as CubeList;
   generateSteam({
-    point: 0,
-    rock,
+    point: [MIN_X, MIN_Y, MIN_Z],
+    droplet,
     steam,
-    MAX_X,
-    MAX_Y,
-    MAX_Z,
+    limits,
   });
+  const steamTotalSA = findSurfaceAreaArray(steam);
 
-  // calculate outer surface area of steam and subtract
-  const steamOuter = 2 * (MAX_X * MAX_Y + MAX_X * MAX_Z + MAX_Y * MAX_Z);
-  const steamTotalSA = findSurfaceArea(steam);
+  // calculate outer surface area of steam
+  const outer = [1 + MAX_X - MIN_X, 1 + MAX_Y - MIN_Y, 1 + MAX_Z - MIN_Z];
+  const steamOuter =
+    2 * (outer[0] * outer[1] + outer[0] * outer[2] + outer[1] * outer[2]);
 
+  // return steam's total surface area minus outer surface area
   return steamTotalSA - steamOuter;
 }
 
 function generateSteam({
   point,
-  rock,
+  droplet,
   steam,
-  MAX_X,
-  MAX_Y,
-  MAX_Z,
+  limits,
 }: {
-  point: number;
-  rock: number[];
-  steam: number[];
-  MAX_X: number;
-  MAX_Y: number;
-  MAX_Z: number;
+  point: CubeList[number];
+  droplet: CubeList;
+  steam: CubeList;
+  limits: {
+    MAX_X: number;
+    MAX_Y: number;
+    MAX_Z: number;
+    MIN_X: number;
+    MIN_Y: number;
+    MIN_Z: number;
+  };
 }) {
-  const POINT_X = Math.floor(point / 10000);
-  const POINT_Y = Math.floor((point % 10000) / 100);
-  const POINT_Z = point % 100;
-
   if (
-    POINT_X >= 0 &&
-    POINT_X < MAX_X &&
-    POINT_Y >= 0 &&
-    POINT_Y < MAX_Y &&
-    POINT_Z >= 0 &&
-    POINT_Z < MAX_Z &&
-    !rock.includes(point) &&
-    !steam.includes(point)
+    point[0] >= limits.MIN_X &&
+    point[0] <= limits.MAX_X &&
+    point[1] >= limits.MIN_Y &&
+    point[1] <= limits.MAX_Y &&
+    point[2] >= limits.MIN_Z &&
+    point[2] <= limits.MAX_Z &&
+    !includesPoint({ list: steam, point }) &&
+    !includesPoint({ list: droplet, point })
   ) {
     steam.push(point);
 
-    generateSteam({ point: point - 10000, rock, steam, MAX_X, MAX_Y, MAX_Z });
-    generateSteam({ point: point + 10000, rock, steam, MAX_X, MAX_Y, MAX_Z });
-    generateSteam({ point: point - 100, rock, steam, MAX_X, MAX_Y, MAX_Z });
-    generateSteam({ point: point + 100, rock, steam, MAX_X, MAX_Y, MAX_Z });
-    generateSteam({ point: point - 1, rock, steam, MAX_X, MAX_Y, MAX_Z });
-    generateSteam({ point: point + 1, rock, steam, MAX_X, MAX_Y, MAX_Z });
+    generateSteam({
+      point: [point[0] - 1, point[1], point[2]],
+      droplet: droplet,
+      steam,
+      limits,
+    });
+    generateSteam({
+      point: [point[0] + 1, point[1], point[2]],
+      droplet: droplet,
+      steam,
+      limits,
+    });
+    generateSteam({
+      point: [point[0], point[1] - 1, point[2]],
+      droplet: droplet,
+      steam,
+      limits,
+    });
+    generateSteam({
+      point: [point[0], point[1] + 1, point[2]],
+      droplet: droplet,
+      steam,
+      limits,
+    });
+    generateSteam({
+      point: [point[0], point[1], point[2] - 1],
+      droplet: droplet,
+      steam,
+      limits,
+    });
+    generateSteam({
+      point: [point[0], point[1], point[2] + 1],
+      droplet: droplet,
+      steam,
+      limits,
+    });
   }
+}
+
+function findSurfaceAreaArray(cubes: CubeList) {
+  let area = 0;
+  for (let i = 0; i < cubes.length; i++) {
+    const cube = cubes[i];
+    let sides = 6;
+    for (let j = 0; j < cubes.length; j++) {
+      if (
+        cube[0] - 1 === cubes[j][0] &&
+        cube[1] === cubes[j][1] &&
+        cube[2] === cubes[j][2]
+      )
+        sides--;
+      if (
+        cube[0] + 1 === cubes[j][0] &&
+        cube[1] === cubes[j][1] &&
+        cube[2] === cubes[j][2]
+      )
+        sides--;
+      if (
+        cube[0] === cubes[j][0] &&
+        cube[1] - 1 === cubes[j][1] &&
+        cube[2] === cubes[j][2]
+      )
+        sides--;
+      if (
+        cube[0] === cubes[j][0] &&
+        cube[1] + 1 === cubes[j][1] &&
+        cube[2] === cubes[j][2]
+      )
+        sides--;
+      if (
+        cube[0] === cubes[j][0] &&
+        cube[1] === cubes[j][1] &&
+        cube[2] - 1 === cubes[j][2]
+      )
+        sides--;
+      if (
+        cube[0] === cubes[j][0] &&
+        cube[1] === cubes[j][1] &&
+        cube[2] + 1 === cubes[j][2]
+      )
+        sides--;
+    }
+    area += sides;
+  }
+  return area;
+}
+
+function includesPoint({
+  list,
+  point,
+}: {
+  list: CubeList;
+  point: CubeList[number];
+}) {
+  return list.some(
+    (cube) =>
+      cube[0] === point[0] && cube[1] === point[1] && cube[2] === point[2]
+  );
 }
 
 export function solve18a(file: string) {
@@ -101,8 +205,14 @@ export function solve18a(file: string) {
 }
 
 export function solve18b(file: string) {
-  const data = parseCubesSingleNum(file);
+  const data = parseCubesArray(file);
   const surfaceArea = findSurfaceAreaWithoutPockets(data);
+  return surfaceArea;
+}
+
+export function testCubeArea(file: string) {
+  const data = parseCubesArray(file);
+  const surfaceArea = findSurfaceAreaArray(data);
   return surfaceArea;
 }
 
@@ -111,4 +221,4 @@ console.log(solve18a("18/input.txt"));
 console.timeEnd("solve18a"); // 16ms
 console.time("solve18b");
 console.log(solve18b("18/input.txt"));
-console.timeEnd("solve18b"); // 180ms
+console.timeEnd("solve18b"); // 530ms
