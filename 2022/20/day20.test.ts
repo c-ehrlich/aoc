b0df8f3683b9ad3af71012fa25691dba3969e50e
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-12-21 00:13:26 +0100

    day 20 part 1

--- 2022/20/day20.test.ts
+++ 2022/20/day20.test.ts
@@ -1,17 +1,55 @@
-import { assert, describe, it } from "vitest";
-import { solve20a, solve20b } from "./day20";
+import { assert, describe, expect, it } from "vitest";
+import {
+  applyDecryptionKey,
+  findGroveCoordinates,
+  parseInput20,
+  shift,
+  solve20a,
+  solve20b,
+} from "./day20";
 
 describe("day20", () => {
   it("input", () => {
-    const sampleData = [0];
-    assert.deepEqual(sampleData, [0]);
+    const sampleData = parseInput20("20/sample.txt");
+    assert.deepEqual(sampleData, inputFixture);
+  });
+  it("shift", () => {
+    const sampleData = parseInput20("20/sample.txt");
+    const shifted = shift(sampleData);
+    assert.deepEqual(
+      shifted.map((num) => num.num),
+      [1, 2, -3, 4, 0, 3, -2]
+    );
+  });
+  it("findGroveCoordinates", () => {
+    assert.equal(findGroveCoordinates([1, 2, -3, 4, 0, 3, -2]), 3);
+  });
+  it("decrypy", () => {
+    const sampleData = parseInput20("20/sample.txt");
+    assert.deepEqual(
+      applyDecryptionKey(sampleData).map((num) => num.num),
+      [
+        811589153, 1623178306, -2434767459, 2434767459, -1623178306, 0,
+        3246356612,
+      ]
+    );
   });
   it("solve20a", () => {
     const sampleData = [0];
-    assert.equal(solve20a(sampleData), 0);
+    assert.equal(solve20a("20/sample.txt"), 3);
   });
   it("solve20b", () => {
     const sampleData = [0];
-    assert.equal(solve20b(sampleData), 0);
+    assert.equal(solve20b("20/sample.txt"), 1623178306);
   });
 });
+
+const inputFixture = [
+  { num: 1, idx: 0 },
+  { num: 2, idx: 1 },
+  { num: -3, idx: 2 },
+  { num: 3, idx: 3 },
+  { num: -2, idx: 4 },
+  { num: 0, idx: 5 },
+  { num: 4, idx: 6 },
+];
--- 2022/20/day20.ts
+++ 2022/20/day20.ts
@@ -1,15 +1,88 @@
 import fs from "fs";
 
-const inputA = [0];
-const inputB = [0];
+type IndexedArr = {
+  num: number;
+  idx: number;
+}[];
 
-export function solve20a(input: number[]) {
-  return 0;
+export function parseInput20(file: string) {
+  return fs
+    .readFileSync(file, "utf8")
+    .split("\n")
+    .map((num, idx) => ({
+      num: parseInt(num),
+      idx,
+    }));
 }
 
-export function solve20b(input: number[]) {
-  return 0;
+export function shift(input: IndexedArr, times = 1) {
+  const arr = [...input];
+
+  for (let i = 0; i < times; i++) {
+    mixOnce(input, arr);
+    console.log(`after ${i + 1} cycles: ${arr.map((arr) => arr.num)}`);
+  }
+
+  return arr;
 }
 
-console.log(solve20a(inputA));
-console.log(solve20b(inputB));
+export function mixOnce(input: IndexedArr, arr: IndexedArr) {
+  input.forEach((number) => {
+    const idx = arr.findIndex((n) => n.idx === number.idx);
+    const num = number.num;
+
+    const cycles =
+      num > 0
+        ? Math.floor((num + idx) / arr.length)
+        : num < 0
+        ? Math.ceil((num - idx) / arr.length)
+        : 0;
+
+    // console.log("---", number.idx, "---");
+    // console.log(idx, arr[idx].num, cycles);
+    let newIdx =
+      (idx + arr[idx].num + cycles + Math.abs(cycles) * arr.length) %
+      arr.length;
+    if (newIdx === 0 && num < 0) newIdx = arr.length - 1;
+
+    // shift the items
+    // console.log("moving ", arr[idx].num, "from", idx, "to", newIdx);
+    const shifted = arr.splice(idx, 1);
+    arr.splice(newIdx, 0, shifted[0]);
+    // console.log(arr.map((arr) => arr.num));
+  });
+}
+
+export function findGroveCoordinates(input: number[]) {
+  const zeroIndex = input.indexOf(0);
+  const number1 = input[(1000 + zeroIndex) % input.length];
+  const number2 = input[(2000 + zeroIndex) % input.length];
+  const number3 = input[(3000 + zeroIndex) % input.length];
+  console.log(number1, number2, number3);
+  return number1 + number2 + number3;
+}
+
+export function applyDecryptionKey(input: IndexedArr) {
+  return input.map((number) => ({
+    ...number,
+    num: number.num * 811589153,
+  }));
+}
+
+export function solve20a(file: string) {
+  const arr = parseInput20(file);
+  const shifted = shift(arr);
+  const groveCoords = findGroveCoordinates(shifted.map((n) => n.num));
+  return groveCoords;
+}
+
+export function solve20b(file: string) {
+  const arr = parseInput20(file);
+  const decrypted = applyDecryptionKey(arr);
+  const shifted = shift(decrypted, 10);
+  const groveCoords = findGroveCoordinates(shifted.map((n) => n.num));
+  return groveCoords;
+}
+
+// console.log(solve20a("20/input.txt"));
+console.log(solve20b("20/sample.txt"));
