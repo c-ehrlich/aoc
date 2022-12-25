cd81ec98f0684ae19f896ed7c43a49db518e7eba
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2022-12-25 09:18:00 +0100

    merry christmas

--- 2022/25/day25.ts
+++ 2022/25/day25.ts
@@ -1,15 +1,45 @@
 import fs from "fs";
 
-const inputA = [0];
-const inputB = [0];
+const snafu = [
+  ["=", -2],
+  ["-", -1],
+  ["0", 0],
+  ["1", 1],
+  ["2", 2],
+];
 
-export function solve25a(input: number[]) {
-  return 0;
+function addSnafuNumbers(file: string): string {
+  const digits = fs
+    .readFileSync(file, "utf8")
+    .split("\n")
+    .map((line) => line.split("").reverse());
+  const sum = [] as number[];
+  for (let i = 0; i < digits.length; i++) {
+    for (let j = 0; j < digits[i].length; j++) {
+      const val = snafu.find((s) => s[0] === digits[i][j])![1];
+      sum[j] = (sum[j] || 0) + (val as number);
+    }
+  }
+  for (let i = 0; i < sum.length; i++) {
+    while (sum[i] < -2) {
+      sum[i] += 5;
+      sum[i + 1] = (sum[i + 1] || 0) - 1;
+    }
+    while (sum[i] > 2) {
+      sum[i] -= 5;
+      sum[i + 1] = (sum[i + 1] || 0) + 1;
+    }
+  }
+  return sum
+    .reverse()
+    .map((digit) => snafu.find((s) => s[1] === digit)![0])
+    .join("");
 }
 
-export function solve25b(input: number[]) {
-  return 0;
+export function solve25a(file: string) {
+  return addSnafuNumbers(file);
 }
 
-console.log(solve25a(inputA));
-console.log(solve25b(inputB));
+console.time("solve25a");
+console.log(solve25a("25/input.txt"));
+console.timeEnd("solve25a"); // 2-3ms
