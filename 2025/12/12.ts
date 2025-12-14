7342c4bc0bc6603a1ea6665578acce692fe5e1f1
Christopher Ehrlich <ehrlich.christopher@gmail.com>
2025-12-14 14:31:05 +0100

    cheesy solution for day 12

--- 2025/12/12.ts
+++ 2025/12/12.ts
@@ -2,9 +2,38 @@ import example from "./example.txt";
 import input from "./input.txt";
 
 export function parseInput(input: string) {
-  return input.split("\n").map(line => line.split(""));
+  const _shapes = input.split("\n\n");
+  const _tasks = _shapes.pop();
+
+  const shapes = _shapes.map(s => {
+    const lines = s.split("\n");
+    lines.shift();
+    return lines.map(l => l.split(""));
+  });
+
+  const tasks = _tasks!.split("\n").map(t => {
+    const [boardRaw, shapesRaw] = t.split(": ");
+    const [x, y] = boardRaw!.split("x").map(Number)!;
+    const shapes = shapesRaw!.split(" ").map(Number);
+    return { size: { x, y }, shapes };
+  });
+
+  return { shapes, tasks };
 }
 
-export function solveA(input: ReturnType<typeof parseInput>) {}
+export function solveA(input: ReturnType<typeof parseInput>) {
+  // dumb solution
+  let sum = 0;
+  for (const task of input.tasks) {
+    const space = task.size.x! * task.size.y!;
+    const presents = task.shapes.reduce((acc, curr) => acc + curr, 0);
+    if (presents * 9 <= space) {
+      ++sum;
+    }
+  }
+  return sum;
+}
 
 export function solveB(input: ReturnType<typeof parseInput>) {}
+
+console.log(solveA(parseInput(input)));
